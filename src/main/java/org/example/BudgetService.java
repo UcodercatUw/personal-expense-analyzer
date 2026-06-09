d41f989eb35c42778b2caf9fa4dc1a5a97a819e5
Alex <aleksandrvastsenko@gmail.com>
2026-06-09 09:52:37 +0300

    Delete src directory

--- src/main/java/org/example/BudgetService.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class BudgetService {
-    public boolean isBudgetExceeded (double currentSpending, double amountToAdd, double budgetLimit) {
-        return (currentSpending + amountToAdd) > budgetLimit;
-    }
-}
--- src/main/java/org/example/ExpenseValidator.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class ExpenseValidator {
-    public boolean isValidAmount(double amount) {
-        return amount > 0;
-    }
-}
